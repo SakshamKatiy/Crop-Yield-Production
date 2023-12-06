8d3a6cbf7b9049b7a02504d5a494cd94ea5025d1
SakshamKatiy <115263557+SakshamKatiy@users.noreply.github.com>
2023-12-06 17:06:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,43 +20,28 @@ average_rain_fall_mm_per_year = st.number_input('Average Rainfall (mm per year)'
 pesticides_tonnes = st.number_input('Pesticides (tonnes)')
 avg_temp = st.number_input('Average Temperature')
 
-area_options = ["Albania", "Algeria", "Angola","Argentina","Bahamas","Bahrain","Bangladesh","Belarus","Canada","Egypt","India","Nepal","New Zealand","Pakistan"]  # Add your actual options here
+area_options = ["Albania", "Algeria", "Angola", "Argentina", "Bahamas", "Bahrain", "Bangladesh", "Belarus", "Canada", "Egypt", "India", "Nepal", "New Zealand", "Pakistan"]  # Add your actual options here
 area = st.selectbox('Area', area_options)
 
-item_option = ["Maize","Potatoes","Wheat","Rice, paddy","Soybeans","Sweet potatoes","Cassava","Yams","Plantains and others"] 
+item_option = ["Maize", "Potatoes", "Wheat", "Rice, paddy", "Soybeans", "Sweet potatoes", "Cassava", "Yams", "Plantains and others"]
 item = st.selectbox('Item', item_option)
 
 if st.button('Predict'):
     if preprocessor is not None:
         # Separate encoding for numerical and categorical features
-        numerical_features = np.array([[year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]])
-        categorical_features = np.array([[area, item]])
+        features = np.array([[year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp, area, item]])
 
         try:
-            # Transform numerical features if available
-            numerical_transformed_features = preprocessor.named_transformers_.get('num', None)
-            if numerical_transformed_features is not None:
-                numerical_transformed_features = numerical_transformed_features.transform(numerical_features)
-            else:
-                raise ValueError("Numerical transformer 'num' not found.")
-
-            # Transform categorical features
-            categorical_transformed_features = preprocessor.named_transformers_.get('cat', None)
-            if categorical_transformed_features is not None:
-                categorical_transformed_features = categorical_transformed_features.transform(categorical_features).toarray()
-            else:
-                raise ValueError("Categorical transformer 'cat' not found.")
-
-            # Combine numerical and categorical features
-            features = np.concatenate([categorical_transformed_features, numerical_transformed_features], axis=1)
-
-            predicted_value = dtr.predict(features).reshape(1, -1)
+            # Transform features
+            transformed_features = preprocessor.transform(features)
+            predicted_value = dtr.predict(transformed_features).reshape(1, -1)
 
             st.markdown('## Predicted Yield Productions:')
             st.write(predicted_value)
 
         except Exception as e:
             st.error(f"Error during prediction: {e}")
+
 
 
 
