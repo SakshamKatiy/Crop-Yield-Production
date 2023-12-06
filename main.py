f54bf231522b2621e4f78fc0dab22a25e23b65e6
SakshamKatiy <115263557+SakshamKatiy@users.noreply.github.com>
2023-12-06 23:23:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
 import streamlit as st
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor
+import pickle
 
-# Assume X_train is your training data
-X_train = np.array([[...]])  # Your actual training data here
+try:
+    # Loading models
+    dtr = pickle.load(open('dtr.pkl', 'rb'))
+    preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
+except Exception as e:
+    st.error(f"Error loading models: {e}")
+    dtr = None
+    preprocessor = None
 
-# Create and fit the preprocessor
-preprocessor = StandardScaler()
-preprocessor.fit(X_train)
+st.title('Crop Yield Prediction Per Country')
 
-# Create and fit the model
-dtr = RandomForestRegressor()
-dtr.fit(X_train, y_train)  # Assuming y_train is your target variable
+st.markdown('## Crop Features Here')
 
-# Streamlit app
-st.title('Yield Production Prediction App')
+year = st.number_input('Year')
+average_rain_fall_mm_per_year = st.number_input('Average Rainfall (mm per year)')
+pesticides_tonnes = st.number_input('Pesticides (tonnes)')
+avg_temp = st.number_input('Average Temperature')
 
-# Input features
-area = st.slider('Area', min_value=0, max_value=1000)
-item = st.selectbox('Item', ['Wheat', 'Corn', 'Rice'])
-year = st.slider('Year', min_value=2000, max_value=2023)
-average_rain_fall_mm_per_year = st.slider('Average Rainfall (mm/year)', min_value=0, max_value=2000)
-pesticides_tonnes = st.slider('Pesticides (tonnes)', min_value=0, max_value=100)
-avg_temp = st.slider('Average Temperature', min_value=-10, max_value=40)
 
-# Prediction button
+area_options = ["Albania", "Algeria", "Angola","Argentina","Bahamas","Bahrain","Bangladesh","Belarus","Canada","Egypt","India","Nepal","New Zealand","Pakistan"]  # Add your actual options here
+
+area = st.selectbox('Area', area_options)
+
+item_option = ["Maize","Potatoes","Wheat","Rice, paddy","Soybeans","Sweet potatoes","Cassava","Yams","Plantains and others"] 
+item= st.selectbox('Item',item_option)
+
 if st.button('Predict'):
-    # Collect input features
     features = np.array([[area, item, year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]])
+    transformed_features = preprocessor.transform(features) 
+    predicted_value = dtr.predict(transformed_features).reshape(1, -1)
 
-    # Transform features using the preprocessor
-    transformed_features = preprocessor.transform(features)
-
-    # Make prediction
-    predicted_value = dtr.predict(transformed_features)
-
-    # Display prediction
     st.markdown('## Predicted Yield Productions:')
     st.write(predicted_value)
 
