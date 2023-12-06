381bc39b7c8cd124b463ee626718ee4ccd123d18
SakshamKatiy <115263557+SakshamKatiy@users.noreply.github.com>
2023-12-07 00:44:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pickle
 try:
     # Loading models
     dtr = pickle.load(open('dtr.pkl', 'rb'))
-    preprocessor = pickle.load(open('preprocesser.pkl', 'rb'))
+    preprocesser = pickle.load(open('preprocesser.pkl', 'rb'))
 except Exception as e:
     st.error(f"Error loading models: {e}")
     dtr = None
@@ -15,30 +15,28 @@ st.title('Crop Yield Prediction Per Country')
 
 st.markdown('## Crop Features Here')
 
-year = st.slider('Year', min_value=2000, max_value=2023)
+year = st.number_input('Year')
 average_rain_fall_mm_per_year = st.number_input('Average Rainfall (mm per year)')
 pesticides_tonnes = st.number_input('Pesticides (tonnes)')
 avg_temp = st.number_input('Average Temperature')
 
-area_options = ["Albania", "Algeria", "Angola", "Argentina", "Bahamas", "Bahrain", "Bangladesh", "Belarus", "Canada", "Egypt", "India", "Nepal", "New Zealand", "Pakistan"]  # Add your actual options here
+
+area_options = ["Albania", "Algeria", "Angola","Argentina","Bahamas","Bahrain","Bangladesh","Belarus","Canada","Egypt","India","Nepal","New Zealand","Pakistan"]  # Add your actual options here
 
 area = st.selectbox('Area', area_options)
 
-item_option = ["Maize", "Potatoes", "Wheat", "Rice, paddy", "Soybeans", "Sweet potatoes", "Cassava", "Yams", "Plantains and others"]
-item = st.selectbox('Item', item_option)
+item_option = ["Maize","Potatoes","Wheat","Rice, paddy","Soybeans","Sweet potatoes","Cassava","Yams","Plantains and others"] 
+item= st.selectbox('Item',item_option)
 
 if st.button('Predict'):
-    raw_features = np.array([[area, item, year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]])
+    features = np.array([[area, item, year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]])
+    tranformed_features = preprocesser.transform(features) 
+    predicted_value = dtr.predict(tranformed_features).reshape(1, -1)
 
-    # Assuming preprocessor is already fitted
-    preprocessed_features = preprocessor.transform(raw_features)
+    st.markdown('## Predicted Yield Productions:')
+    st.write(predicted_value)
 
-    try:
-        predicted_value = dtr.predict(preprocessed_features)
-        st.markdown('## Predicted Yield Productions:')
-        st.write(predicted_value)
-    except Exception as e:
-        st.error(f"Error predicting: {e}")
+
 
 
 
