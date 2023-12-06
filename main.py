649d528c24ed625a16d89e7ecde73dae8738114c
SakshamKatiy <115263557+SakshamKatiy@users.noreply.github.com>
2023-12-07 00:29:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,7 @@ item= st.selectbox('Item',item_option)
 
 if st.button('Predict'):
     features = np.array([[area, item, year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]])
-    
-    # Add print statements for debugging
-    print(f"Original Features: {features}")
-    
-    transformed_features = preprocessor.transform(features) 
-    predicted_value = dtr.predict(transformed_features).reshape(1, -1)
+    predicted_value = dtr.predict(features)
 
     st.markdown('## Predicted Yield Productions:')
     st.write(predicted_value)
