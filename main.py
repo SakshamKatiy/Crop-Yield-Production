d42958148ffb20e8e04d5bcc15169cf1193d41a2
SakshamKatiy <115263557+SakshamKatiy@users.noreply.github.com>
2023-12-06 23:13:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,35 +20,22 @@ average_rain_fall_mm_per_year = st.number_input('Average Rainfall (mm per year)'
 pesticides_tonnes = st.number_input('Pesticides (tonnes)')
 avg_temp = st.number_input('Average Temperature')
 
-area_options = ["Albania", "Algeria", "Angola", "Argentina", "Bahamas", "Bahrain", "Bangladesh", "Belarus", "Canada", "Egypt", "India", "Nepal", "New Zealand", "Pakistan"]
+
+area_options = ["Albania", "Algeria", "Angola","Argentina","Bahamas","Bahrain","Bangladesh","Belarus","Canada","Egypt","India","Nepal","New Zealand","Pakistan"]  # Add your actual options here
+
 area = st.selectbox('Area', area_options)
 
-item_option = ["Maize", "Potatoes", "Wheat", "Rice, paddy", "Soybeans", "Sweet potatoes", "Cassava", "Yams", "Plantains and others"]
-item = st.selectbox('Item', item_option)
+item_option = ["Maize","Potatoes","Wheat","Rice, paddy","Soybeans","Sweet potatoes","Cassava","Yams","Plantains and others"] 
+item= st.selectbox('Item',item_option)
 
 if st.button('Predict'):
-    if preprocessor is not None:
-        # Separate encoding for numerical and categorical features
-        features = np.array([[year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp, area, item]])
+    features = np.array([[area, item, year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]])
+    transformed_features = preprocessor.transform(features) 
+    predicted_value = dtr.predict(transformed_features).reshape(1, -1)
 
-        try:
-            print("Original Features:", features)
+    st.markdown('## Predicted Yield Productions:')
+    st.write(predicted_value)
 
-            # Transform features
-            transformed_features = preprocessor.transform(features)
-            print("Transformed Features:", transformed_features)
-
-            predicted_value = dtr.predict(transformed_features).reshape(1, -1)
-
-            st.markdown('## Predicted Yield Productions:')
-            st.write(predicted_value)
-
-        except Exception as e:
-            st.error(f"Error during prediction: {e}")
-
-# Add these lines to print information about the loaded models
-print("Decision Tree Regressor:", dtr)
-print("Preprocessor:", preprocessor)
 
 
 
