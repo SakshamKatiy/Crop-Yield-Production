de35ed764fbccf440b9a8e42df3b9e74a75ed4d5
SakshamKatiy <115263557+SakshamKatiy@users.noreply.github.com>
2023-12-06 17:14:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ average_rain_fall_mm_per_year = st.number_input('Average Rainfall (mm per year)'
 pesticides_tonnes = st.number_input('Pesticides (tonnes)')
 avg_temp = st.number_input('Average Temperature')
 
-area_options = ["Albania", "Algeria", "Angola", "Argentina", "Bahamas", "Bahrain", "Bangladesh", "Belarus", "Canada", "Egypt", "India", "Nepal", "New Zealand", "Pakistan"]  # Add your actual options here
+area_options = ["Albania", "Algeria", "Angola", "Argentina", "Bahamas", "Bahrain", "Bangladesh", "Belarus", "Canada", "Egypt", "India", "Nepal", "New Zealand", "Pakistan"]
 area = st.selectbox('Area', area_options)
 
 item_option = ["Maize", "Potatoes", "Wheat", "Rice, paddy", "Soybeans", "Sweet potatoes", "Cassava", "Yams", "Plantains and others"]
@@ -32,8 +32,12 @@ if st.button('Predict'):
         features = np.array([[year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp, area, item]])
 
         try:
+            print("Original Features:", features)
+
             # Transform features
             transformed_features = preprocessor.transform(features)
+            print("Transformed Features:", transformed_features)
+
             predicted_value = dtr.predict(transformed_features).reshape(1, -1)
 
             st.markdown('## Predicted Yield Productions:')
@@ -41,6 +45,11 @@ if st.button('Predict'):
 
         except Exception as e:
             st.error(f"Error during prediction: {e}")
+
+# Add these lines to print information about the loaded models
+print("Decision Tree Regressor:", dtr)
+print("Preprocessor:", preprocessor)
+
 
 
 
